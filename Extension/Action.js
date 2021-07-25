81f101558cc14cd32af30495322284db67698131
Anmol Kalra <anmolkalra17@gmail.com>
2021-07-25 12:26:37 +0530

    Added extension functionality for running custom JavaScript code

--- Extension/Action.js
+++ Extension/Action.js
@@ -3,11 +3,12 @@ var Action = function() { }
 Action.prototype = {
 	
 run: function(parameters) {
-	
+	parameters.completionFunction({"URL": document.URL, "title": document.title});
 },
 
 finalize: function(parameters) {
-	
+	var customJS = parameters["customJavaScript"];
+	eval(customJS);
 }
 
 };
